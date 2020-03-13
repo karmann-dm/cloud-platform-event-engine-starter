4acb7b8b3cc24719b136aab29f144b4478cb4477
karmann-dm <karmanno@gmail.com>
2020-03-13 14:57:55 +0300

    redeveloped beans structure

--- src/main/java/com/karmanno/cloudplatform/events/EventPublishConfigurer.java
+++ src/main/java/com/karmanno/cloudplatform/events/EventPublishConfigurer.java
@@ -3,9 +3,7 @@ package com.karmanno.cloudplatform.events;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
 
-@Component
 public class EventPublishConfigurer {
     private final EventsProperties eventsProperties;
     private final KafkaTemplate<String, String> kafkaTemplate;
--- src/main/java/com/karmanno/cloudplatform/events/EventsConfiguration.java
+++ src/main/java/com/karmanno/cloudplatform/events/EventsConfiguration.java
@@ -1,10 +1,12 @@
 package com.karmanno.cloudplatform.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @Configuration
 @AutoConfigureAfter(KafkaAutoConfiguration.class)
@@ -16,5 +18,16 @@ public class EventsConfiguration {
         EventPublishContainer container = new EventPublishContainer();
         eventPublishConfigurer.configure(container);
         return container;
+    }
+
+    @Bean
+    public EventPublishConfigurer eventPublishConfigurer(EventsProperties eventsProperties,
+                                                         KafkaTemplate<String, String> kafkaTemplate,
+                                                         ObjectMapper objectMapper) {
+        return new EventPublishConfigurer(
+                eventsProperties,
+                kafkaTemplate,
+                objectMapper
+        );
     }
 }
