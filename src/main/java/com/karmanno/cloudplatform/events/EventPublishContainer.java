911e7268d31b6693530cb09f5ea5836803378ebf
karmann-dm <karmanno@gmail.com>
2020-03-13 15:10:18 +0300

    fix: NPE fix

--- src/main/java/com/karmanno/cloudplatform/events/EventPublishContainer.java
+++ src/main/java/com/karmanno/cloudplatform/events/EventPublishContainer.java
@@ -1,9 +1,10 @@
 package com.karmanno.cloudplatform.events;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class EventPublishContainer {
-    private Map<Class<?>, EventPublisher> transferEventPublisherMap;
+    private Map<Class<?>, EventPublisher> transferEventPublisherMap = new HashMap<>();
 
     public void publishEvent(Event event) {
         transferEventPublisherMap.get(event.getClass()).publish(event);
